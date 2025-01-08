95bb528fdfd17d2a50656eadbe36cdc8705afcb7
Barry Sande <barrysandebbs@gmail.com>
2025-01-08 18:07:27 +0300

    neat!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,17 +2,20 @@
 	import About from '$lib/components/About.svelte';
 	import Skills from '$lib/components/Skills.svelte';
 	import Projects from '$lib/components/Projects.svelte';
-	import {fade, fly} from 'svelte/transition';
-	
+	import { fade, fly } from 'svelte/transition';
 </script>
 
 <section
 	class="mx-6 flex flex-col items-center justify-center gap-10 p-8 subpixel-antialiased md:flex-row md:justify-start lg:mx-20"
 >
-	<div >
+	<div>
 		<h1 class="text-nowrap py-6 text-green-500">Hello, my name is</h1>
-		
-		<h2 class=" text-nowrap pb-2 text-4xl font-bold text-zinc-600 dark:text-zinc-300 md:text-5xl" in:fly={{x:-200, duration: 2000}} out:fade>
+
+		<h2
+			class=" text-nowrap pb-2 text-4xl font-bold text-zinc-600 dark:text-zinc-300 md:text-5xl"
+			in:fly={{ x: -200, duration: 2000 }}
+			out:fade
+		>
 			Barry Sande
 		</h2>
 		<p class=" pb-3 text-xl font-semibold text-zinc-600 dark:text-zinc-300">Front End Developer</p>
@@ -34,4 +37,3 @@
 		<Projects />
 	</section>
 </main>
-

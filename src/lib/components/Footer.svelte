ea2fcc9917b016191f6415bd89e2323cdd8e8406
Barry Sande <barrysandebbs@gmail.com>
2026-04-21 17:26:17 +0300

    fix footer.

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,15 +1,20 @@
-<footer class="mt-16 border-t border-border">
-	<div class="mx-2 flex flex-col gap-4 px-8 py-6 md:mx-6 md:flex-row md:items-center md:justify-between lg:mx-20">
-		<div class="flex flex-col gap-3 sm:flex-row sm:items-center sm:gap-6">
-			<p class="font-mono text-xs text-ink-muted">
-				© 2026 Barry Sande — all rights reserved
-			</p>
+<script>
+	import Icon from '@iconify/svelte';
+</script>
+
+<footer class="border-border mx-2 mt-16 gap-4 border-t px-8 py-6 md:mx-6 lg:mx-20">
+	<div class="items-cente flex flex-col justify-between gap-3 md:flex-row md:gap-5">
+		<div class="flex gap-0.5">
+			<Icon icon="solar:copyright-line-duotone" class=" size-3" />
+			<p class="text-ink-muted font-mono text-xs">2026 Barry Sande — all rights reserved</p>
+		</div>
+		<div class="flex items-center justify-between gap-4">
 			<div class="flex items-center gap-4">
 				<a
 					href="https://github.com/barrysande"
 					target="_blank"
 					rel="noopener noreferrer"
-					class="font-mono text-xs text-ink-muted transition-colors hover:text-primary"
+					class="text-ink-muted hover:text-primary font-mono text-xs underline underline-offset-2 transition-colors"
 				>
 					GitHub
 				</a>
@@ -17,15 +22,13 @@
 					href="https://www.linkedin.com/in/barry-sande-8703b8312/"
 					target="_blank"
 					rel="noopener noreferrer"
-					class="font-mono text-xs text-ink-muted transition-colors hover:text-primary"
+					class="text-ink-muted hover:text-primary flex items-center gap-0.5 font-mono text-xs underline underline-offset-2 transition-colors"
 				>
 					LinkedIn
 				</a>
 			</div>
+			<div class="text-ink-muted hidden items-center md:flex">|</div>
+			<p class="text-ink-muted font-mono text-xs tracking-widest uppercase">Nairobi / Remote</p>
 		</div>
-
-		<p class="font-mono text-xs uppercase tracking-widest text-ink-muted">
-			Nairobi / Remote
-		</p>
 	</div>
 </footer>
